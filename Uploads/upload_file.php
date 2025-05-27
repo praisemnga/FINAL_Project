ff4da339c153b472c82608200783efd80b16058a
VaniaSaleletang07 <vaniasaleletang026@student.unsrat.ac.id>
2025-05-28 03:41:19 +0700

    menambahkan folder uploads

--- Uploads/upload_file.php
+++ Uploads/upload_file.php
@@ -13,11 +13,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_FILES['lampiran'])) {
     if ($task && $file['error'] === UPLOAD_ERR_OK) {
         $ext = pathinfo($file['name'], PATHINFO_EXTENSION);
         $newName = uniqid('lampiran_', true) . '.' . $ext;
-        $target = __DIR__ . '/uploads/' . $newName;
+        $target = __DIR__ . '/Uploads/' . $newName;
 
         if (move_uploaded_file($file['tmp_name'], $target)) {
             // Simpan info ke database
-            $mysqli = new mysqli("localhost", "root", "", "tugas_project_akhir");
+            // $mysqli = new mysqli("localhost", "root", "", "tugas_project_akhir");
             $stmt = $mysqli->prepare("INSERT INTO attachments (task_name, filename, uploaded_by) VALUES (?, ?, ?)");
             $stmt->bind_param("sss", $task, $newName, $user);
             $stmt->execute();
