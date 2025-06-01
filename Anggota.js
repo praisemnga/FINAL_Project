d47124a9eeff96b9d459033b87c13ba7fcec8a73
VaniaSaleletang07 <vaniasaleletang026@student.unsrat.ac.id>
2025-06-02 00:12:04 +0700

    menambahkan foto dan memperbaiki tampilan foto anggota kelompok

--- Anggota.js
+++ Anggota.js
@@ -4,12 +4,14 @@ const anggota = [
     nim: "230211060088",
     email: "praisemoniaga026@student.unsrat.ac.id",
     telp: "0821-9131-5664",
+    foto: "images/praise.jpg" // ganti dengan nama file foto kamu
   },
   {
     nama: "VANIA ESTHER SALELETANG",
     nim: "230211060012",
     email: "vaniasaleletang026@student.unsrat.ac.id",
     telp: "0813-4214-9692",
+    foto: "images/vania.jpg" // ganti dengan nama file foto anggota
   },
   // Tambahkan anggota lain sesuai kelompokmu
 ];
@@ -18,6 +20,7 @@ const anggotaList = document.getElementById('anggotaList');
 anggota.forEach(a => {
   const li = document.createElement('li');
   li.innerHTML = `
+    <img class="anggota-foto" src="${a.foto}" alt="Foto ${a.nama}" onerror="this.src='https://ui-avatars.com/api/?name=${encodeURIComponent(a.nama)}&background=4f46e5&color=fff'">
     <span class="anggota-nama">${a.nama}</span>
     <span class="anggota-nim">NIM: ${a.nim}</span>
     <span class="anggota-email">Email: ${a.email}</span>
