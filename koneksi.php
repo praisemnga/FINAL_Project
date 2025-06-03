286183e1a7d636ec6defb8e6c4621db63d577a95
VaniaSaleletang07 <vaniasaleletang026@student.unsrat.ac.id>
2025-06-03 21:06:59 +0700

    merubah koneksi sesuai dengan hosting

--- koneksi.php
+++ koneksi.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "root", "", "tugas_project_akhir");
+$mysqli = new mysqli("sql209.infinityfree.com", "if0_39149676", "Projectweb123", "if0_39149676_tugas_project_akhir");
 if ($mysqli->connect_error) {
     die("Gagal koneksi MySQL: " . $mysqli->connect_error);
 }
